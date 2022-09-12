4d5533b9ef9504e1f3cb1346c6c899f94b5c9216
mukhliskurniaaji <mukhlisk93@gmail.com>
2022-09-13 05:47:29 +0700

    Updates

---
+++ precache-manifest.20b889abba76c95b0758230c16145dd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c44e6f2658e224b659e6",
+    "url": "/react-redux-thunk-axios/static/css/main.9619dcba.chunk.css"
+  },
+  {
+    "revision": "c44e6f2658e224b659e6",
+    "url": "/react-redux-thunk-axios/static/js/main.c44e6f26.chunk.js"
+  },
+  {
+    "revision": "c68ce5a453f3fd946c82",
+    "url": "/react-redux-thunk-axios/static/js/1.c68ce5a4.chunk.js"
+  },
+  {
+    "revision": "a18f748284b507ec9808",
+    "url": "/react-redux-thunk-axios/static/js/runtime~main.a18f7482.js"
+  },
+  {
+    "revision": "9da327e8fc4241ddf28295963585e9c8",
+    "url": "/react-redux-thunk-axios/index.html"
+  }
+];
